07e90a5ee1349c07c362ceef75244c5cac01bb14
TMoiseyenko06 <tmoiseyenko06@gmail.com>
2024-08-02 09:53:04 -0400

    Initial Commit

---
+++ advanced_list_methods.py
@@ -0,0 +1,5 @@
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if 'Alice' in submitted and 'Alice' in attended:
+    print('Alice has subbmited the assigment and attended the last class!')
---
+++ python_list_transformation.py
@@ -0,0 +1,11 @@
+ 
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+#Task 1:
+grades.sort(reverse=True)
+#Task 2:
+average_grade = sum(grades) / len(grades)
+
+print("Sorted List:", grades)
+print("Average Grade:", average_grade)
+
+
